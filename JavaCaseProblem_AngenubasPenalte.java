fdbc23e8e010e2024d587108e247369b4da50d77
anjen4143 <penalteangenubas@gmail.com>
2025-10-17 15:28:23 +0800

    Add files via upload
    Uploaded java case problem and output screenshot

---
+++ JavaCaseProblem_AngenubasPenalte.java
@@ -0,0 +1,18 @@
+public class  JavaCaseProblem_AngenUbasPenalte {
+    public static void main(String[] args) {
+        // Display personal information
+        System.out.println("Name: Angen U. Penalte");
+        System.out.println("ID Number: 2024303239");
+        System.out.println("----------------------------------");
+
+        int grade = 85;
+
+        if (grade >= 75) {
+            System.out.println("Result: Your grade is " + grade + " - You Passed!");
+        } else {
+            System.out.println("Result: Your grade is " + grade + " - You Failed!");
+        }
+    }
+}
+		
+	
